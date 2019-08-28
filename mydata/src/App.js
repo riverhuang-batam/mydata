27b61ddd8ae2c589fd60c1584a4ad95a3e02dd3e
river <riverhuang011@gmail.com>
2019-08-28 10:56:09 +0700

    create react app

--- mydata/src/App.js
+++ mydata/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends Component{
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,5 @@ function App() {
       </header>
     </div>
   );
+  }
 }
-
-export default App;
