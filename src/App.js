13e5a337e7137007f20a62bd471129c0a42b182b
river <riverhuang011@gmail.com>
2019-08-28 11:49:03 +0700

    data and button for the download install reactstrap bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import {Container, Row, Col, Button} from 'reactstrap'
+import RiverPdf from './CVRiverApple.pdf'
+import Riverpdfoto from './CVRiverApple.png'
+import RiverPhoto from './me.jpg'
+import motiveletter from './MotiveLetter.png'
+import motiveletterpdf from './MotiveLetter.pdf'
 
 export default class App extends Component{
   render(){
   return (
-    <div className="App">
-      testtest
+    <div>
+    <Container>
+    <div className="text-center mt-4 mb-4">
+      <h1 style={{color:"red"}}>River Data</h1>
+      </div>
+    <Row>
+      <Col sm="4">
+    <div className="text-center">
+    <img src={Riverpdfoto} width="70%"/>
+    <br/>
+    <br/>
+    <Button color="primary" href={RiverPdf} download>
+      Download
+    </Button>
     </div>
+
+    </Col>
+    <Col sm="4">
+    <div className="text-center">
+    <img src={RiverPhoto} width="65%"/>
+    <br/>
+    <br/>
+    <Button color="primary" className="align-item-center" href={RiverPhoto} download>Download</Button>
+    </div>
+    </Col>
+    <Col sm="4">
+      <div className="text-center mb-4">
+      <img src={motiveletter} width="70%"/>
+      <br/>
+      <br/>
+    <Button color="primary" href={motiveletterpdf} download>Download</Button>
+    </div>
+    
+    </Col>
+    </Row>
+    </Container>
+  </div>
   );
   }
 }
